4ca76324d19990c6f76c829d87981b89b286bcaa
Felix Enrique Leon Molina <felixenriquelm45@gmail.com>
2019-08-30 12:53:45 -0700

    sube

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Bici bici= new Bici();
+        bici.init("Gallito", 13, 12);
+        bici.sube();
+        bici.print();
+        }
+    }
+
+    class Bici {
+        
+        private string marca; private int current_velocity;
+        private int velocities;
+        public void print()
+        {
+             Console.WriteLine("Marca:{0}, Velocity:{1}", marca, current_velocity);
+        }
+        
+          
+        public void init(string marca, int cv, int vs)
+        {
+            this.marca = marca;
+            current_velocity = cv;
+            velocities = vs; 
+
+            if (cv>vs)
+            {
+                current_velocity = vs;
+            }
+               
+            else
+            {
+                current_velocity = cv;
+            }
+               
+        }
+        
+    }
+}
